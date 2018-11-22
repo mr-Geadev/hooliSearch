6ba4bf80ff63169d8daad5f4a3355d75138c8fa6
Kozlov Georgiy <kgk2409@gmail.com>
2018-11-22 15:51:34 +0400

    Добавил сортировку списка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { merge, Observable, Subject } from 'rxjs';
+import { combineLatest, merge, Observable, Subject } from 'rxjs';
 import {
   debounceTime,
   distinctUntilChanged,
+  filter,
   map,
   mapTo,
   publishReplay,
@@ -35,23 +36,52 @@ export class AppComponent {
   );
 
   // кнопка поиска
-  readonly searchButton = new Subject<boolean>();
+  readonly searchButton = new Subject<void>();
   // поток, который немедленно отправляет текущее значение инпута как query параметр
   readonly searchButton$: Observable<string> = this.searchButton.asObservable().pipe(
+    filter(() => this.search.value), // не отправлять значение для поиска если строка пустая
     map(() => {
       return this.search.value;
     }),
     share(),
   );
 
-  // поток списка ссылок. Делает запрос когда лиюо произошло нажатие на кнопку, либо пользователь ничего не вводит какое-то время
-  readonly listLinks$: Observable<Link[]> = merge(
-    this.textInput$,
-    this.searchButton$,
+  // кнопка сортировки
+  readonly sortButton = new Subject<string>();
+  // поток содержащий в себе тип сортировки
+  readonly sortedList$: Observable<string> = this.sortButton.asObservable().pipe(
+    startWith(null),
+    share(),
+  );
+
+  // поток списка ссылок. Представляет из себя комбинацию потоков типа сортировки и списка ссылок.
+  readonly listLinks$: Observable<Link[]> = combineLatest(
+    // этот поток получает новый список при обновлении поискового запроса
+    merge(
+      this.textInput$,
+      this.searchButton$,
+    ).pipe(
+      distinctUntilChanged(),
+      switchMap((val) => {
+        return this.server.getLinks(val);
+      }),
+    ),
+    this.sortedList$,
   ).pipe(
-    distinctUntilChanged(),
-    switchMap(val => {
-      return this.server.getLinks(val);
+    // после чего происходит сортировка
+    map(([list, sort]) => {
+      if (sort) {
+        return list.sort((a, b) => {
+          if (a[sort] < b[sort]) {
+            return -1;
+          }
+          if (a[sort] > b[sort]) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      return list;
     }),
     publishReplay(1), // отправляет последнее значение к вновь подписавшимся обзерверам
     refCount(),
@@ -69,5 +99,4 @@ export class AppComponent {
 
   constructor(readonly server: ServerService) {
   }
-
 }
